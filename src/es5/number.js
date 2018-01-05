adc817e09b600ae3e7e66046e687d7329352edf5
Fabio Soares <fabioars@live.com>
2018-01-05 15:08:27 -0300

    Remove randomInt Function

--- src/es5/number.js
+++ src/es5/number.js
@@ -1,76 +1,45 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
-/**
- *
- * @param {array} input
- * @param {array} output
- */
 var range = exports.range = function range(input, output) {
-  return function (value) {
-    return output[0] + (value - input[0]) * (output[1] - output[0]) / (input[1] - input[0]);
-  };
+    return function (value) {
+        return output[0] + (value - input[0]) * (output[1] - output[0]) / (input[1] - input[0]);
+    };
 };
 
-/**
- * FIXME: To use const
- * @param {array} pts
- */
 var bezier = exports.bezier = function bezier(pts) {
-  return function (t) {
-    var a = pts,
-        b = [],
-        i = 0,
-        j = 0;
+    return function (t) {
+        var a = pts,
+            b = [],
+            i = 0,
+            j = 0;
 
-    for (a = pts; a.length > 1; a = b) {
-      for (i = 0, b = [], j; i < a.length - 1; i++) {
-        for (b[i] = [], j = 0; j < a[i].length; j++) {
-          b[i][j] = a[i][j] * (1 - t) + a[i + 1][j] * t;
-        }
-      }
-    }return a[0];
-  };
+        for (a = pts; a.length > 1; a = b) {
+            for (i = 0, b = [], j; i < a.length - 1; i++) {
+                for (b[i] = [], j = 0; j < a[i].length; j++) {
+                    b[i][j] = a[i][j] * (1 - t) + a[i + 1][j] * t;
+                }
+            }
+        }return a[0];
+    };
 };
 
-/**
- *
- * @param {number} min
- * @param {number} max
- */
 var random = exports.random = function random(min, max) {
-  return Math.random() * (max - min) + min;
+    var int = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
+
+    var num = Math.random() * (max - min) + min;
+    return !int ? num : Math.floor(num);
 };
 
-/**
- *
- * @param {number} min
- * @param {number} max
- */
-var randomInt = exports.randomInt = function randomInt(min, max) {
-  return Math.floor(random(min, max));
-};
-
-/**
- *
- * @param {number} min
- * @param {number} max
- */
 var randomGenerator = exports.randomGenerator = function randomGenerator(min, max) {
-  return function () {
-    return random(min, max);
-  };
+    return function () {
+        var int = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
+        return random(min, max, int);
+    };
 };
 
-/**
- *
- * @param {number} value
- * @param {number} start
- * @param {number} end
- * @returns {boolean}
- */
 var inRange = exports.inRange = function inRange(value, start, end) {
-  return value >= start && value <= end;
+    return value >= start && value <= end;
 };
--- src/number.js
+++ src/number.js
@@ -1,16 +1,8 @@
-/**
- *
- * @param {array} input
- * @param {array} output
- */
+
 export const range = (input, output) => value =>
     output[0] +
     (value - input[0]) * (output[1] - output[0]) / (input[1] - input[0]);
 
-/**
- * FIXME: To use const
- * @param {array} pts
- */
 export const bezier = pts => {
     return t => {
         let a = pts,
@@ -27,32 +19,11 @@ export const bezier = pts => {
     };
 };
 
-/**
- *
- * @param {number} min
- * @param {number} max
- */
-export const random = (min, max) => Math.random() * (max - min) + min;
+export const random = (min, max, int = false) => {
+    const num = Math.random() * (max - min) + min;
+    return !int ? num : Math.floor(num);
+};
 
-/**
- *
- * @param {number} min
- * @param {number} max
- */
-export const randomInt = (min, max) => Math.floor(random(min, max));
+export const randomGenerator = (min, max) => (int = false) => random(min, max, int);
 
-/**
- *
- * @param {number} min
- * @param {number} max
- */
-export const randomGenerator = (min, max) => () => random(min, max);
-
-/**
- *
- * @param {number} value
- * @param {number} start
- * @param {number} end
- * @returns {boolean}
- */
 export const inRange = (value, start, end) => value >= start && value <= end;
